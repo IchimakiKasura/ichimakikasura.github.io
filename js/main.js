bd95609d291a3527563fa17856f5c37b9bc3f957
Ichimaki <ichimakiKasura@gmail.com>
2021-11-22 01:54:04 +0800

    commitment are the best

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
+か("body").sty("overflow", "hidden");
+
 document.addEventListener("DOMContentLoaded", ()=>{
+
     setTimeout(()=>{
         か(".preLoad").sty("opacity", 0);
     }, 1000)
+
     setTimeout(()=>{
         か(".preLoad").sty("display", "none");
     }, 1350)
+
+    setTimeout(()=>{
+        if(か(window).width('inner') > 1400) か(".navbar").sty("top", "20px");
+        else か(".navbar").sty("top", "0px");
+    }, 1400)
+
+    setTimeout(()=>{
+        か(".navbar").sty("position", "sticky");
+        か("body").sty("overflow", "auto");
+    }, 2400)
 })
+
+か(window).resize(()=>{
+    if(か(window).width('inner') > 1400) か(".navbar").sty("top", "20px");
+    else か(".navbar").sty("top", "0px");
+});
