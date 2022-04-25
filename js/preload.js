85afd8a44ecd45894972348f9b36a20163540e14
Ichimaki <ichimakiKasura@gmail.com>
2022-04-26 01:32:58 +0800

    Dev Commit

---
+++ js/preload.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const sections = ["main", "about", "project", "contact", "footer"];
+let int = 0;
+
+(function(){
+    let section = location.hash.replace("#","");
+    switch(section)
+    {
+        case "main": return int = 0;
+        case "about": return int = 1;
+        case "project": return int = 2;
+        case "contact": return int = 3;
+        case "footer": return int = 4;
+    }
+})();
+
+window.onload = ()=>{
+    let preload = setTimeout(()=>{
+        か(".preLoad").sty("opacity", 0);
+    }, 1000)
+
+    let preloadTwo = setTimeout(()=>{
+        か(".preLoad").sty("display", "none");
+    }, 1350)
+
+};
+
+var WebScroll = function(e){
+    
+    if(e.wheelDelta < 0)
+    {
+
+        // scroll down
+        if(int >= 4) return;
+        int++;
+        location.href = `index.html#${sections[int]}`
+        
+    } else {
+        
+        //scroll up
+        if(int == 0) return;
+        int--;
+        location.href = `index.html#${sections[int]}`
+
+    }
+    removeEventListener('wheel', WebScroll, false);
+
+    setTimeout(()=>{
+        console.log(int)
+        addEventListener('wheel', WebScroll, false);
+    }, 500);
+};
+
+addEventListener('wheel', WebScroll, false);
