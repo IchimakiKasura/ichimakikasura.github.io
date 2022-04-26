14b055f088076f99f2233dff5b143b0ab9be164d
Ichimaki <ichimakiKasura@gmail.com>
2022-04-27 02:37:49 +0800

    Dev Commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,81 @@
 "use strict";
 
-document.addEventListener("contextmenu",function(event){
+const nav_copy = (arg)=>navigator.clipboard.writeText(arg.getSelection())
+let btn = か("#button").gt;
+let sectionMouseOver = か("[ismousehovered]").gAll;
+let CopyButton = か("[menu-copy]");
+
+// #region web scroll thing
+sectionMouseOver.forEach(element => {
+    element.addEventListener("mouseleave", ()=>{ isMouseHover = false });
+    element.addEventListener("mouseover", ()=>{ isMouseHover = true });
+});
+
+か(document).evt("keydown", (e)=>{ if(e.ctrlKey) isKeyDown = true;})
+か(document).evt("keyup", (e)=>{ isKeyDown = false;})
+
+//#endregion
+
+CopyButton.evt("mouseover",()=>{
+    CopyButton.sty("background", "var(--ctx-hover-bg)")
+})
+CopyButton.evt("mouseleave",()=>{
+    if(!window.getSelection().isCollapsed)
+        CopyButton.sty("background", "transparent")
+})
+
+
+か(document).evt("contextmenu",function(event){
     event.preventDefault();
-    var ctxMenu = か(".vertical-menu").gt;
-    // ctxMenu.style.display = "block";
+    let ctxMenu = か(".vertical-menu").gt;
     ctxMenu.style.opacity = 1;
     ctxMenu.style.visibility = "visible";
     ctxMenu.style.left = (event.pageX)+"px";
     ctxMenu.style.top = (event.pageY)+"px";
-},false);
 
-document.addEventListener("mousedown",function(event){
-    var ctxMenu = か(".vertical-menu").gt;
+    if(window.getSelection().isCollapsed)
+    {
+        CopyButton.sty("background", "var(--ctx-hover-bg)")
+        CopyButton.sty("cursor", "default")
+        return;
+    }
+
+    CopyButton.sty("background", "transparent")
+    CopyButton.sty("cursor", "pointer")
+
+});
+
+か(document).evt("mousedown",function(){
+    let ctxMenu = か(".vertical-menu").gt;
     ctxMenu.style.visibility = "hidden";
     ctxMenu.style.opacity = 0;
-},false);
+});
+
+か("[title='Home']").evt("click", ()=>{
+    window.open("https://github.com/IchimakiKasura");
+});
+
+か("[mobile-nav-menu]").evt("change", (e)=>{
+    if(e.target.checked)
+    {
+        か(".mobile-nav").sty("top", "0px");
+        return;
+    }
+
+    か(".mobile-nav").sty("top", "-150vh");
+});
+
+か(window).resize(()=>{
+    location.href = location.hash;
+})
+
+か(window).evt("visibilitychange", ()=>{
+    if (document.visibilityState == "visible")
+        CanScroll = true;
+})
+
+function removeNavMobile()
+{
+    か("[mobile-nav-menu]").gt.checked = false;
+    か(".mobile-nav").sty("top", "-150vh");
+}
--- js/preload.js
+++ js/preload.js
@@ -2,11 +2,13 @@
 
 const sections = ["main", "about", "projects", "contact", "footer"];
 let int = 0;
+let CanScroll = true;
+let isMouseHover = false;
+let isKeyDown = false;
 
-(function(){
-    let section = location.hash.replace("#","");
-    switch(section)
-    {
+(function () {
+    let section = location.hash.replace("#", "");
+    switch (section) {
         case "main": return int = 0;
         case "about": return int = 1;
         case "projects": return int = 2;
@@ -15,48 +17,75 @@ let int = 0;
     }
 })();
 
-window.onload = ()=>{
-    setTimeout(()=>{
+window.onload = () => {
+
+    if (か.isMobile) か("body").gt.style.overflowY = "visible";
+
+    setTimeout(() => {
         か(".preLoad").sty("opacity", 0);
         か(".nav").sty("top", "0");
     }, 1000)
-    
-    setTimeout(()=>{
+
+    setTimeout(() => {
         か(".preLoad").sty("display", "none");
         か("[ScrollArrow]").sty("display", "block");
     }, 1350)
 
-    setTimeout(()=>{
+    setTimeout(() => {
         か("[ScrollArrow]").sty("display", "block");
     }, 3000)
+
+
 };
 
-var WebScroll = function(e){
-    
+// #region web scroll thing
+か(window).scroll(() => {
+    if (か.isMobile) location.hash = null;
+    if (window.pageYOffset > 300) {
+        btn.classList.add("show");
+    } else {
+        btn.classList.remove("show");
+    }
+})
+
+か(document).evt('wheel', (e) => {
+
+    // console.table({"mouse": isMouseHover,"scroll": CanScroll,"key": isKeyDown})
+    if (か.isMobile) return;
+    if (!isMouseHover) return;
+    if (!CanScroll) return;
+    if (isKeyDown) return;
+
     var ctxMenu = か(".vertical-menu").gt;
     ctxMenu.style.visibility = "hidden";
     ctxMenu.style.opacity = 0;
 
-    if(e.wheelDelta < 0)
-    {
+    if (e.wheelDelta < 0) {
         // scroll down
-        if(int >= 4) return;
-        int++;
-        location.href = `#${sections[int]}`
-        
+        scrollDownPage();
+        CanScroll = false;
     } else {
-        
+
         //scroll up
-        if(int == 0) return;
-        int--;
-        location.href = `#${sections[int]}`
-
+        scrollUpPage();
+        CanScroll = false;
     }
-    removeEventListener('wheel', WebScroll, false);
 
-    setTimeout(()=>{
-        addEventListener('wheel', WebScroll, false);
-    }, 950);
-};
+    setTimeout(() => {
+        CanScroll = true;
+    }, 700);
 
-addEventListener('wheel', WebScroll, false);
+});
+//#endregion
+
+function scrollDownPage() {
+    if (int >= 4) return;
+    int++;
+    location.href = `#${sections[int]}`
+}
+
+function scrollUpPage() {
+    if (int == 0) return;
+    int--;
+    location.href = `#${sections[int]}`
+}
